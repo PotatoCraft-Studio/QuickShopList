d89118ceab0eda74413d3193b51b035e0b9c3652
Ghost_chu <2908803755@qq.com>
2020-07-30 20:42:08 +0800

    Adding the ability to allow player to see others shop list

--- src/main/java/com/mcsunnyside/quickshop/list/quickshoplist/ListCommand.java
+++ src/main/java/com/mcsunnyside/quickshop/list/quickshoplist/ListCommand.java
@@ -3,6 +3,7 @@ package com.mcsunnyside.quickshop.list.quickshoplist;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
@@ -13,11 +14,13 @@ import org.maxgamer.quickshop.api.QuickShopAPI;
 import org.maxgamer.quickshop.command.CommandProcesser;
 import org.maxgamer.quickshop.shop.Shop;
 import org.maxgamer.quickshop.util.ItemNMS;
+import org.maxgamer.quickshop.util.MsgUtil;
 import org.maxgamer.quickshop.util.Util;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 
 public class ListCommand implements CommandProcesser {
     private QuickShopList plugin;
@@ -32,8 +35,18 @@ public class ListCommand implements CommandProcesser {
             commandSender.sendMessage("This command cannot running by Console.");
             return;
         }
+        UUID playerToSee;
+        if (strings.length < 1) {
+            playerToSee = ((Player) commandSender).getUniqueId();
+        } else {
+            if (!commandSender.hasPermission("quickshop.list.others")) {
+                commandSender.sendMessage(MsgUtil.getMessage("no-permission", commandSender));
+                return;
+            }
+            playerToSee = Bukkit.getOfflinePlayer(strings[0]).getUniqueId();
+        }
         Player player = (Player) commandSender;
-        List<Shop> shops = QuickShopAPI.getShopAPI().getPlayerAllShops(player.getUniqueId());
+        List<Shop> shops = QuickShopAPI.getShopAPI().getPlayerAllShops(playerToSee);
         player.sendMessage(plugin.getConfig().getString("lang.prefix").replace("{total}", String.valueOf(shops.size())));
         if (shops.isEmpty()) {
             player.sendMessage(plugin.getConfig().getString("lang.nothing"));
